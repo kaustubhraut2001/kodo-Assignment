52d272af272fe75bc7d12ecd59c83b6938bba9be
Kaustubh <kaustubhr2001@gmail.com>
2024-04-14 22:21:27 +0530

    fixes some stylings for movies component

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -4,20 +4,22 @@ const Movie = ({ movie }) => {
   return (
     <>
       {console.log(movie, "movies in moview componet")}
-      <div className="bg-white p-4 rounded shadow">
+      <div className="bg-white p-4 rounded shadow-md">
         <img
           src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
           alt={movie?.title}
-          className="rounded"
+          className="rounded-t-md mb-4"
         />
 
         <h2 className="text-xl font-bold mb-2">{movie?.title}</h2>
-        <p className="text-gray-700">{movie?.overview}</p>
-        <p className="text-gray-700">{movie?.vote_average}</p>
-        <p className="text-gray-700">{movie?.vote_count}</p>
-        <p className="text-gray-700">{movie?.popularity}</p>
+        <p className="text-gray-700 mb-2">{movie?.overview}</p>
+        <p className="text-gray-700 mb-2">Rating: {movie?.vote_average}</p>
+        <p className="text-gray-700 mb-2">Votes: {movie?.vote_count}</p>
+        <p className="text-gray-700 mb-2">Popularity: {movie?.popularity}</p>
+        <p className="text-gray-700 mb-2">
+          Release Date: {movie?.release_date}
+        </p>
 
-        <p className="text-gray-700">{movie?.release_date}</p>
         <button className="bg-blue-500 text-white px-4 py-2 rounded mt-2">
           View Details
         </button>
