2a7325b8f01f60e75f4f6aa85182d26a8f28c820
Kaustubh <kaustubhr2001@gmail.com>
2024-04-14 23:32:00 +0530

    movies nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Movie from "./Components/Movie";
 import MovieList from "./Components/MovieList";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <MovieList />
     </>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 text-white px-4 py-2 sticky">
+      <div className="container mx-auto flex items-center justify-center">
+        <div className="font-semibold text-xl">Movies App 🎬</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
