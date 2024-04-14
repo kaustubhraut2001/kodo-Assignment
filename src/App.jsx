9dd7aa235a0543d3be42a48150de0eb4fa306c44
Kaustubh <kaustubhr2001@gmail.com>
2024-04-14 21:44:50 +0530

    setup the react vite app and tailwind css created hook to fetch data

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Movie from "./Components/Movies";
+
+function App() {
+  return (
+    <>
+      <Movie />
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/usefetchdata.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+const ENDPOINT = "/discover/movie";
+
+const fetchMovies = async (page) => {
+  try {
+    const response = await axios.get(`${BASE_URL}${ENDPOINT}`, {
+      params: {
+        api_key: API_KEY,
+        page: page,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
+};
+
+export { fetchMovies };
